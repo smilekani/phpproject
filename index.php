2eb7d2280eb255bf50caa3597f9adf459d8d0ec4
kanimozhi <kanimozhi@DESKTOP-99B8DAG>
2017-09-21 23:39:48 +0530

    Search button added in index.php

--- index.php
+++ index.php
@@ -2,12 +2,39 @@
 <html lang="en">
 <head>
   <title>kani Tutorial</title>
+  <form id="searchForm" action="javascript:search();">
+   <div class="input-group">
+      <button id="go" class="btn btn-default" type="button" 
+              onclick="document.getElementById('searchForm').submit(); return false;">
+      </button>
+      <input type="text" id="searchItem" class="form-control" placeholder="Suchbegriff">
+   </div>
+</form>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="css/trs.css">
   <script src="jquery/jquery-3.2.1.min.js"></script>
   <script src="bootstrap/js/bootstrap.min.js"></script>
+  <script>
+function search() {
+ 
+   var name = document.getElementById("searchForm").elements["searchItem"].value;
+   var pattern = name.toLowerCase();
+   var targetId = "";
+ 
+   var divs = document.getElementsByClassName("col-md-2");
+   for (var i = 0; i < divs.length; i++) {
+      var para = divs[i].getElementsByTagName("p");
+      var index = para[0].innerText.toLowerCase().indexOf(pattern);
+      if (index != -1) {
+         targetId = divs[i].parentNode.id;
+         document.getElementById(targetId).scrollIntoView();
+         break;
+      }
+   }  
+}
+</script>
 </head>
 <body>
 <div class="menu">
