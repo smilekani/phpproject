ece32110deab189859f44c45df48cdfe638692a9
Kanimozhi M <kanimozhi1390@gmail.com>
2017-11-24 17:30:32 +0530

    additions

--- apache/menu.php
+++ apache/menu.php
@@ -10,7 +10,28 @@
       <li><a href="../java/javabasics.php">Java</a></li>
 	  <li><a href="../grewords/lettera.php">GRE</a></li>
 	  <li><a href="../english/englishgrammerbasics.php">English</a></li>
-	  <li><a href="../html/htmlintro.php">HTML</a></li>
+	  <li><a href="../apache/apachebasics.php">Apache</a></li>
+	  <li><a href="../biology/biologybasics.php">Biology</a></li>
+	  <li><a href="../botany/botanybasics.php">HTML</a></li>
+	  <li><a href="../BPM/bpmbasics.php">HTML</a></li>
+	  <li><a href="../c/cbasics.php">HTML</a></li>
+	  <li><a href="../c++/cpluscplusbasics.php">HTML</a></li>
+	  <li><a href="../civics/civicsbasics.php">HTML</a></li>
+	  <li><a href="../chemistry/chemistrybasics.php">HTML</a></li>
+	  <li><a href="../eclipse/eclipsebasics.php">HTML</a></li>
+	  <li><a href="../embedded/emdeddedbasics.php">HTML</a></li>
+	  <li><a href="../eclipse/eclipsebasics.php">HTML</a></li>
+	  <li><a href="../eclipse/eclipsebasics.php">HTML</a></li>
+	  <li><a href="../eclipse/eclipsebasics.php">HTML</a></li>
+	  <li><a href="../eclipse/eclipsebasics.php">HTML</a></li>
+	  <li><a href="../eclipse/eclipsebasics.php">HTML</a></li>
+	  <li><a href="../eclipse/eclipsebasics.php">HTML</a></li>
+	  <li><a href="../eclipse/eclipsebasics.php">HTML</a></li>
+	  <li><a href="../eclipse/eclipsebasics.php">HTML</a></li>
+	  <li><a href="../eclipse/eclipsebasics.php">HTML</a></li>
+	  <li><a href="../eclipse/eclipsebasics.php">HTML</a></li>
+	  <li><a href="../eclipse/eclipsebasics.php">HTML</a></li>
+	  <li><a href="../eclipse/eclipsebasics.php">HTML</a></li>
     </ul>
   </div>
 </nav>
