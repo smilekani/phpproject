1aa7ea599cff62165dbb01d097ccfbae94e743af
Kanimozhi M <kanimozhi1390@gmail.com>
2017-11-27 18:45:11 +0530

    Changes.

--- history/historybasics.php
+++ history/historybasics.php
@@ -12,38 +12,9 @@
 		</div>
     </div>
     <div class="col-sm-8">
-      <h3>English Usage</h3>
-	  <h2>What is the Oxford comma?</h2>
-      <p class="normal" class="normal">TThe presence or lack of a comma before and or or in a list of 
-      three or more items is the subject of much debate. Such a comma is known as a serial comma. 
-      For a century it has been part of Oxford University Press style to retain or impose this last comma consistently,
-       to the extent that the convention has also come to be called the Oxford comma. 
-       However, the style is also used by many other publishers, both in the UK and elsewhere. 
-       Examples of the serial comma are:</p>
-	  </br><p class="normal">mad, bad, and dangerous to know
-a thief, a liar, and a murderer
-a government of, by, and for the people</p>
-	  <br><p class="normal">Consistency and comma confusion</p>
-	  <br><p class="normal">The general rule is that one style or the other should be used consistently. 
-	  However, the last comma can serve to resolve ambiguity, particularly when any of the items are compound terms 
-	  joined by a conjunction, and it is sometimes helpful to the reader to use an isolated 
-	  serial comma for clarification even when the convention has not been adopted in the rest of the text. In</p>
-	  <br><p class="normal">cider, real ales, meat and vegetable pies, and sandwiches</p>
-	  <br><p class="normal">the absence of a comma after pies would imply something unintended about the sandwiches.
-	   In the next example, it is obvious from the grouping afforded by the commas that the Bishop of Bath and Wells is one person,
-	    and the bishops of Bristol, Salisbury, and Winchester are three people:</p>
-	   <br><p class="normal">the bishops of Bath and Wells, Bristol, Salisbury, and Winchester</p>
-	   <br><p class="normal">If the order is reversed to become</p>
-	   <br><p class="normal">the bishops of Winchester, Salisbury, Bristol, and Bath and Wells</p>
-	   <br><p class="normal">then the absence of the comma after Bristol would generate ambiguity: is the link between Bristol and Bath rather than Bath and Wells?</p>
-	   <br><p class="normal">In a list of three or more items, use a comma before a final extension phrase such as etc., and so forth, and the like:</p>
-	   <br><p class="normal">potatoes, swede, carrots, turnips, etc. candles, incense, vestments, and the like</p>
-	   <br><p class="normal">It is important to note that only elements that share a relationship with the introductory material should be linked in this way. In:</p>
-	   <br><p class="normal">the text should be lively, readable, and have touches of humour</p>
-	   <br><p class="normal">only the first two elements fit syntactically with the text should be; the sentence should rather be written:</p>
-	   <br><p class="normal">the text should be lively and readable, and have touches of humour</p>
-	   <br><p class="normal">For More Info Punctuation Please follow this link:</p>
-	   <a href="https://en.oxforddictionaries.com/grammar/punctuation">English Grammer Punctuations</a>
+      <h3>History</h3>
+	  <h2>What is History?</h2>
+      <p class="normal" class="normal">Uses of History</p>
 	 <div class="row">
 	  <div class="col-sm-2">
 	  <form action="javabasic.php" method="post">
